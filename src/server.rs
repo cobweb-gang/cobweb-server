fc7a00bd05dd52dcb8b2091e279c15264d45f510
David Bernado <dbernado@protonmail.com>
2019-02-10 17:46:49 -0800

    fix handshake

--- src/server.rs
+++ src/server.rs
@@ -7,7 +7,8 @@ use std::net::SocketAddr;
 use std::time::Duration;
 use std::io::Error;
 
-// Add client_addr argument
+// The handshake is fucked up
+// You are forgetting to receive the inbound_msg
 pub fn handshake(client_num: &i32, server_id: &str, sock: &UdpSocket, pass: &str, client_addr: &SocketAddr) -> Result<(Key, SocketAddr), String> {
     let (spake, outbound_msg) = SPAKE2::<Ed25519Group>::start_b(
         &Password::new(pass.as_bytes()),
@@ -15,7 +16,6 @@ pub fn handshake(client_num: &i32, server_id: &str, sock: &UdpSocket, pass: &str
         &Identity::new(server_id.as_bytes()),
         );
 
-    let inbound_msg: &mut [u8] = &mut [0u8];
     FutureRetry::new(|| sock.send_to(outbound_msg.as_slice(), client_addr).into_future(), |_| {
         eprintln!("ERROR: Unable to connect to client. Trying again...");
         RetryPolicy::<Error>::WaitRetry(Duration::new(5, 0))
@@ -23,7 +23,15 @@ pub fn handshake(client_num: &i32, server_id: &str, sock: &UdpSocket, pass: &str
         .wait()
         .unwrap();
 
-    let key = spake.finish(&inbound_msg).unwrap();
+    let inbound_msg: &mut [u8] = &mut [0u8];
+    FutureRetry::new(|| sock.recv(inbound_msg).into_future(), |_| {
+        eprintln!("ERROR: Unable to connect to client. Trying again...");
+        RetryPolicy::<Error>::WaitRetry(Duration::new(5, 0))
+    })
+        .wait()
+        .unwrap();
+    
+    let key = spake.finish(inbound_msg).unwrap();
     let key_pass = format!("{:?}", key);
     
     Ok((Key::from_pw(KeyType::Aes128, &key_pass, server_id), *client_addr))
